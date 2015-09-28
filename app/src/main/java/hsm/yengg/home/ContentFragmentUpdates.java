19f1cbbdda04e02c7b54ef96ea5ada83c3645883
farukh <farukh9311@gmail.com>
2015-09-28 10:05:01 +0530

    Navigation header added

---
+++ app/src/main/java/hsm/yengg/home/ContentFragmentUpdates.java
@@ -0,0 +1,21 @@
+package hsm.yengg.home;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ummarul on 22/9/15.
+ */
+public class ContentFragmentUpdates extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_fragment_updates,container,false);
+        return v;
+    }
+}
--- app/src/main/java/hsm/yengg/home/HomeActivity.java
+++ app/src/main/java/hsm/yengg/home/HomeActivity.java
@@ -1,5 +1,6 @@
 package hsm.yengg.home;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,13 +12,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class HomeActivity extends AppCompatActivity {
     Toolbar toolbar;
     String titles[]={"News & Updates","Syllabus","Rules & Regulations","Academic Calendar","Join with us"};
-    int icons[]={R.drawable.update,R.drawable.syllabus,R.drawable.rules_regulations,R.drawable.calendar,R.drawable.join_with_us};
-
+    //int icons[]={R.drawable.update,R.drawable.syllabus,R.drawable.rules_regulations,R.drawable.calendar,R.drawable.join_with_us};
+    public NavigationView navigationView;
     private Toolbar mToolbar;
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
@@ -33,22 +35,90 @@ public class HomeActivity extends AppCompatActivity {
         toolbar=(Toolbar)findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
+        navigationView = (NavigationView) findViewById(R.id.navigation);
 
-        recyclerView=(RecyclerView)findViewById(R.id.RecyclerView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+
+                drawerLayout.closeDrawers();
+
+
+                switch (menuItem.getItemId()) {
+
+
+
+                    case R.id.updates:
+                        Toast.makeText(getApplicationContext(), "News & updates Secleted", Toast.LENGTH_SHORT).show();
+                        ContentFragmentUpdates fragment = new ContentFragmentUpdates();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.frame,fragment);
+                        fragmentTransaction.commit();
+                        return true;
+
+
+
+                    case R.id.academic_calendar:
+                        Toast.makeText(getApplicationContext(), "Academic Calendar Selected", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.join:
+                        Toast.makeText(getApplicationContext(), "Please Join With Us Selected", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    default:
+                        Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+            }
+        });
+
+       /*recyclerView=(RecyclerView)findViewById(R.id.RecyclerView);
         recyclerView.setHasFixedSize(true);
 
         adapter=new NavigationDrawerAdapter(titles,icons);
         recyclerView.setAdapter(adapter);
 
         layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(layoutManager);*/
 
-        drawerLayout=(DrawerLayout)findViewById(R.id.drawer);
+        /*drawerLayout=(DrawerLayout)findViewById(R.id.drawer);
         drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer,R.string.closeDrawer){
         };
 
         drawerLayout.setDrawerListener(drawerToggle);
-        drawerToggle.syncState();
+        drawerToggle.syncState();*/
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
 
 
 
